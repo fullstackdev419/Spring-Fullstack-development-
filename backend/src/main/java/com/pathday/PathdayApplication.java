59f0270a27d23a6b22a87b6fcdaef08e18de2e55
fullstackdev419 <isto.realdev1105@gmail.com>
2021-08-19 23:16:09 +0200

    Create project backend

---
+++ backend/src/main/java/com/pathday/PathdayApplication.java
@@ -0,0 +1,13 @@
+package com.pathday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathdayApplication.class, args);
+    }
+
+}
